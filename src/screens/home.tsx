caeffb70adf8574b52ec3b957aac07a3fabfe600
arhamabeer <arhamabeerahmed@gmail.com>
2023-07-20 18:11:55 +0500

    GetAllAnimals integrated

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,46 +1,54 @@
 import React, { useEffect, useState } from "react";
 import StatusCard from "../components/dashboardStatusCard";
+import { instance } from "../api";
 
-const data = [
-  {
-    label: "Total Animals",
-    quantity: 251,
-  },
-  {
-    label: "Total Cows",
-    quantity: 151,
-  },
-  {
-    label: "Total Goats",
-    quantity: 99,
-  },
-  {
-    label: "Total Camels",
-    quantity: 1,
-  },
-];
-
-type Count = [
-  {
-    label: string;
-    quantity: number;
-  }
-];
+type Count =
+  | [
+      {
+        label: string;
+        quantity: number;
+      }
+    ]
+  | [];
 
 function Home() {
-  const [animalCount, setAnimalCount] = useState<Count>([
-    {
-      label: "",
-      quantity: 0,
-    },
-  ]);
+  const [animalCount, setAnimalCount] = useState<Count>([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    (async () => {
+      let response = await instance.get("/GetAllAnimals");
+      if (response.status === 200) {
+        //set data to state
+        let label = Object.keys(response.data.data);
+        let quantity: Array<number> = Object.values(response.data.data);
+        let total_quantity = 0;
+        let data: [{ label: string; quantity: number }] = [
+          {
+            label: "total animals".toUpperCase(),
+            quantity: 0,
+          },
+        ];
+        for (let i = 0; i < label.length; i++) {
+          let obj: { label: string; quantity: number } = {
+            label: label[i].toUpperCase(),
+            quantity: quantity[i],
+          };
+          total_quantity += quantity[i];
+          data.push(obj);
+        }
+        data[0].quantity = total_quantity;
+        setAnimalCount(data);
+      } else {
+      }
+    })();
+  }, []);
+
   return (
     <div className="flex flex-wrap m-3 w-full">
-      {data.map((item, key) => (
-        <StatusCard label={item.label} quantity={item.quantity} key={key} />
-      ))}
+      {animalCount.length > 0 &&
+        animalCount.map((item, key) => (
+          <StatusCard label={item.label} quantity={item.quantity} key={key} />
+        ))}
     </div>
   );
 }
