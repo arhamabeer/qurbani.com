b68476f1f82ec1f0319a999342c12dbf452f11ce
kemmboss <arhamabeerahmed@gmail.com>
2023-07-23 02:18:45 +0500

    Integrated  GetAnimalNumberAvailableForRegisteration API

--- src/screens/registerAnimal.tsx
+++ src/screens/registerAnimal.tsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import AuthBanner from "../components/authBanner";
 import { instance } from "../api";
 
-const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 type Animal = {
   animalId: number;
   type: string;
@@ -11,9 +9,22 @@ type Animal = {
   parts: number;
   animalDetails: [];
 };
+type Data = {
+  type: number;
+  number: number;
+  partPrice: number;
+  desc: string;
+};
 
 function RegisterAnimal() {
   const [animals, setAnimals] = useState<Animal[]>([]);
+  const [number, setNumber] = useState<number[]>([]);
+  const [data, setData] = useState<Data>({
+    type: 0,
+    desc: "",
+    number: 0,
+    partPrice: 0,
+  });
 
   useEffect(() => {
     (async () => {
@@ -25,15 +36,56 @@ function RegisterAnimal() {
       }
     })();
   }, []);
-  console.log(animals);
+
+  const handleTypeChange = async (animalId: number) => {
+    let response = await instance.get(
+      "/GetAnimalNumberAvailableForRegisteration",
+      {
+        params: {
+          AnimalId: animalId,
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      setNumber(response.data.data);
+      setData((prevState) => ({
+        ...prevState,
+        type: animalId,
+      }));
+    }
+  };
+
+  const handleNumberChange = async (number: number) => {
+    setData((prevState) => ({
+      ...prevState,
+      number: number,
+    }));
+  };
+  const handlePriceChange = async (price: number) => {
+    setData((prevState) => ({
+      ...prevState,
+      partPrice: price,
+    }));
+  };
+  const handleDescChange = async (desc: string) => {
+    setData((prevState) => ({
+      ...prevState,
+      desc: desc,
+    }));
+  };
+  console.log(data);
 
   if (animals.length === 0) return <h1>LOADING....</h1>;
   return (
     <div className="flex  justify-between">
       <div className="flex w-2/4 flex-col justify-center items-center">
         <div className="w-2/4 flex justify-center items-center my-2 h-10 border border-themeBgDark rounded-xl px-2">
-          <select className="h-full w-full bg-transparent text-themeBg placeholder-themeBgPlaceholder">
-            <option defaultChecked selected disabled value={0}>
+          <select
+            className="h-full w-full bg-transparent text-themeBg placeholder-themeBgPlaceholder"
+            onChange={(e) => handleTypeChange(parseInt(e.target.value))}
+          >
+            <option defaultChecked defaultValue={0} selected disabled value={0}>
               Animal Type
             </option>
             {animals.map((animal) => (
@@ -48,8 +100,10 @@ function RegisterAnimal() {
             name="day"
             className="h-full w-full bg-transparent text-themeBg"
             id=""
+            // disabled={number.length < 0}
+            onChange={(e) => handleNumberChange(parseInt(e.target.value))}
           >
-            <option defaultChecked disabled value="">
+            <option defaultValue={0} selected disabled value="">
               Animal Number
             </option>
             {number.map((num) => (
@@ -64,6 +118,7 @@ function RegisterAnimal() {
             type="number"
             className="h-full w-full bg-transparent text-themeBg placeholder-themeBgPlaceholder"
             placeholder="Part Price"
+            onChange={(e) => handlePriceChange(parseInt(e.target.value))}
           />
         </div>
         <div className="w-2/4 flex justify-center items-center my-2 h-auto border border-themeBgDark rounded-xl px-2">
@@ -71,6 +126,7 @@ function RegisterAnimal() {
             rows={3}
             className="h-auto w-full bg-transparent text-themeBg placeholder-themeBgPlaceholder"
             placeholder="Any info about the animal"
+            onChange={(e) => handleDescChange(e.target.value)}
           />
         </div>
         <button className="w-1/4 flex justify-center items-center my-2 h-10  bg-themeBgDark rounded-xl px-2">
