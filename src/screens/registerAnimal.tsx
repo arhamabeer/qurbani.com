6a367f884320133ca2a1344427878076af7aa6a0
kemmboss <arhamabeerahmed@gmail.com>
2023-07-26 23:43:19 +0500

    loader added on registerAnimal

--- src/screens/registerAnimal.tsx
+++ src/screens/registerAnimal.tsx
@@ -10,7 +10,6 @@ import Loader from "../components/Loader";
 function RegisterAnimal() {
   const [animals, setAnimals] = useState<Animal[]>([]);
   const [number, setNumber] = useState<number[]>([]);
-  const [load, setLoad] = useState<boolean | null>(null);
   const [data, setData] = useState<RegisterAnimalData>({
     type: 0,
     desc: "",
@@ -87,74 +86,81 @@ function RegisterAnimal() {
     }
   };
 
-  if (animals.length > 0) <Loader />;
-  return (
-    <div className="flex  justify-between">
-      <div className="flex w-2/4 flex-col justify-center items-center">
-        <div className="w-2/4 flex justify-center items-center my-2 h-10 border border-themeBgDark rounded-xl px-2">
-          <select
-            className="h-full w-full bg-transparent text-themeBg placeholder-themeBgPlaceholder"
-            onChange={(e) => handleTypeChange(parseInt(e.target.value))}
-          >
-            <option defaultChecked defaultValue={0} selected disabled value={0}>
-              Animal Type
-            </option>
-            {animals.map((animal) => (
-              <option key={animal.animalId} value={animal.animalId}>
-                {animal.type}
+  if (animals.length === 0) return <Loader />;
+  else
+    return (
+      <div className="flex  justify-between">
+        <div className="flex w-2/4 flex-col justify-center items-center">
+          <div className="w-2/4 flex justify-center items-center my-2 h-10 border border-themeBgDark rounded-xl px-2">
+            <select
+              className="h-full w-full bg-transparent text-themeBg placeholder-themeBgPlaceholder"
+              onChange={(e) => handleTypeChange(parseInt(e.target.value))}
+            >
+              <option
+                defaultChecked
+                defaultValue={0}
+                selected
+                disabled
+                value={0}
+              >
+                Animal Type
               </option>
-            ))}
-          </select>
-        </div>
-        <div className="w-2/4 flex justify-center items-center my-2 h-10 border border-themeBgDark rounded-xl px-2">
-          <select
-            name="day"
-            className="h-full w-full bg-transparent text-themeBg"
-            id=""
-            // disabled={number.length < 0}
-            onChange={(e) => handleNumberChange(parseInt(e.target.value))}
-          >
-            <option defaultValue={0} selected disabled value="">
-              Animal Number
-            </option>
-            {number.map((num) => (
-              <option key={num} value={num}>
-                {num}
+              {animals.map((animal) => (
+                <option key={animal.animalId} value={animal.animalId}>
+                  {animal.type}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="w-2/4 flex justify-center items-center my-2 h-10 border border-themeBgDark rounded-xl px-2">
+            <select
+              name="day"
+              className="h-full w-full bg-transparent text-themeBg"
+              id=""
+              // disabled={number.length < 0}
+              onChange={(e) => handleNumberChange(parseInt(e.target.value))}
+            >
+              <option defaultValue={0} selected disabled value="">
+                Animal Number
               </option>
-            ))}
-          </select>
+              {number.map((num) => (
+                <option key={num} value={num}>
+                  {num}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="w-2/4 flex justify-center items-center my-2 h-10 border border-themeBgDark rounded-xl px-2">
+            <input
+              type="number"
+              className="h-full w-full bg-transparent text-themeBg placeholder-themeBgPlaceholder"
+              placeholder="Part Price"
+              onChange={(e) => handlePriceChange(e.target.value)}
+            />
+          </div>
+          <div className="w-2/4 flex justify-center items-center my-2 h-auto border border-themeBgDark rounded-xl px-2">
+            <textarea
+              rows={3}
+              className="h-auto w-full bg-transparent text-themeBg placeholder-themeBgPlaceholder"
+              placeholder="Any info about the animal"
+              onChange={(e) => handleDescChange(e.target.value)}
+            />
+          </div>
+          <button
+            className="w-1/4 flex justify-center items-center my-2 h-10  bg-themeBgDark rounded-xl px-2"
+            onClick={() => submitToast(submit)}
+          >
+            ADD
+          </button>
         </div>
-        <div className="w-2/4 flex justify-center items-center my-2 h-10 border border-themeBgDark rounded-xl px-2">
-          <input
-            type="number"
-            className="h-full w-full bg-transparent text-themeBg placeholder-themeBgPlaceholder"
-            placeholder="Part Price"
-            onChange={(e) => handlePriceChange(e.target.value)}
-          />
-        </div>
-        <div className="w-2/4 flex justify-center items-center my-2 h-auto border border-themeBgDark rounded-xl px-2">
-          <textarea
-            rows={3}
-            className="h-auto w-full bg-transparent text-themeBg placeholder-themeBgPlaceholder"
-            placeholder="Any info about the animal"
-            onChange={(e) => handleDescChange(e.target.value)}
-          />
-        </div>
-        <button
-          className="w-1/4 flex justify-center items-center my-2 h-10  bg-themeBgDark rounded-xl px-2"
-          onClick={() => submitToast(submit)}
-        >
-          ADD
-        </button>
+        <AuthBanner
+          action={9}
+          text="Add a new Animal in the Stocks"
+          title="Register Animal"
+        />
+        <ToastComponent />
       </div>
-      <AuthBanner
-        action={9}
-        text="Add a new Animal in the Stocks"
-        title="Register Animal"
-      />
-      <ToastComponent />
-    </div>
-  );
+    );
 }
 
 export default RegisterAnimal;
